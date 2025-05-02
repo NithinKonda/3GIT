3ad61ae7795f142f571149a75d26fc1f49538626
NithinKonda <nithinkonda142@gmail.com>
2025-05-02 21:41:57 +0530

    add recharts dependency and update App component layout to include ContributionStats

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import GithubContributionHeatmap from "./components/GitHeatMap";
+import ContributionStats from "./components/ContributionStats";
 function App() {
 
   return (
     <>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold mb-6 text-gray-800">GitHub Contribution Stats</h1>
       <GithubContributionHeatmap />
+      <ContributionStats />
+      </div>
     </>
   );
 }
